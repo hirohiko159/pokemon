464d343ff25aa77c87d82ed49db79f0e1e4c6292
ThomasP-MTL <106160906+ThomasP-MTL@users.noreply.github.com>
2022-05-27 16:48:36 +0200

    Update bdd_V1.py

--- bdd_V1.py
+++ bdd_V1.py
@@ -12,22 +12,26 @@ class Bdd ():
             self.data_joueurs=json.load(f)
     #methode pour récupérer un pokemon aleatoire
     def get_random(self):
-        return random.choice(self.data_pokedex)
-      
+        return random.choice(self.data_pokedex) 
     #methode récupérer un joueur en fonction d'un nom
-  
-    #methode "add_Bdd" qui doit ajouter un joueur dans le dictionnaire // un joueur est un dictionnaire de dictionnaire
-       #methode pour creer joueur
-        def create_joueur(self, joueur)
-            joueur={"name": joueur,"Pokemon1": self.get_random(),"Pokemon2": self.get_random(),"Pokemon3": self.get_random()} 
-          
+    def retrieve_joueur(self, joueur)
+        get_attr(self.data_joueurs,joueur)
+    #methode pour creer joueur
+    def create_joueur(self, nom_joueur)
+        joueur={
+          "name": nom_joueur,
+          "Pokemon1": self.get_random(),
+          "Pokemon2": self.get_random(),
+          "Pokemon3": self.get_random()
+        } 
+    #methode "add_Bdd" qui doit ajouter un joueur dans le dictionnaire // un joueur est un dictionnaire de dictionnaire  
     def add_joueur(self, joueur, adversaire)
-      Bdd.joueur.append(Bdd(adversaire{"Pokemon1", "Pokemon2", "Pokemon3"})
-      humain.humains.append(humain(random.choice(humain.prenoms),random.choice(humain.sexe)))
+        Bdd.joueur.append(Bdd(adversaire{"Pokemon1", "Pokemon2", "Pokemon3"})
+        humain.humains.append(humain(random.choice(humain.prenoms),random.choice(humain.sexe)))
     
     #methode supprimer un joueur 
-    def remove(self,joueur):
-       Bdd.remove(joueur)
+    def remove(self,nom_joueur):
+        Bdd.joueur.remove(nom_joueur)
     #methode changer un joueur (pour ajouter/supprimer pokemon selon le resultat du combat determiné par pokemon.py)
     #methode enregistrer qui enregistre le dictionnaire self.data dans le fichier json
 
